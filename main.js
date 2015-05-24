475b6fdf55ffe13b664f15caf44fab91a370847a
Asifa <faizanbss@rocketmail.com>
2015-05-24 01:33:50 -0700

    First

---
+++ main.js
@@ -0,0 +1,10 @@
+//var string : = 2;
+//console.log (string)
+//string = "34"; 
+var myString = "test";
+var myNumber = 1;
+var myBoolean = true;
+myString = myNumber;
+myBoolean = myString;
+myNumber = myBoolean;
+//# sourceMappingURL=main.js.map
---
+++ main.ts
@@ -0,0 +1,14 @@
+////var string : = 2;
+////console.log (string)
+////string = "34"; 
+//var	myString	=	"test";
+//var	myNumber	=	1; 
+//var	myBoolean	=	true; 
+//myString	=	myNumber;
+//myBoolean	=	myString;
+//myNumber	=	myBoolean; 
+//
+//thanks 
+var	complexType	={	name:	"myName",	id:	1	}; 
+complexType	=	{ name:	"anotherName"	};
+
